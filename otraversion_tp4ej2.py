a15c6b7d65ba8fc8e50153e5f109efd6ae35d6d0
lauravelazquez25 <83258190+lauravelazquez25@users.noreply.github.com>
2021-05-27 11:48:08 -0300

    Add files via upload
    @martinvilu
    
    no caigo todavia..no estoy escribiendo bien, no se como usar la funcion suma_lenta

---
+++ otraversion_tp4ej2.py
@@ -0,0 +1,20 @@
+################
+# Laura Velazquez - @lauravelazquez25
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Suma lenta
+def suma_lenta(numero, otro_numero):
+    while contador< otro_numero:
+        contador = contador +1
+        suma = numero + contador
+        return suma
+
+
+numero = int(input('ingrese un numero: '))
+otro_numero = int(input('ingrese otro numero: '))
+print ('la suma lenta es: ',suma)
+pass
+
+if __name__ == "__main__":
+    prueba()
