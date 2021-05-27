eaec04ff1c167ba3977cea7ca64cec5b1a8cd0c8
lauravelazquez25 <83258190+lauravelazquez25@users.noreply.github.com>
2021-05-26 21:27:24 -0300

    Add files via upload

---
+++ tp4ej2.py
@@ -0,0 +1,19 @@
+################
+# Laura Velazquez - @lauravelazquez25
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Suma lenta
+def suma_lenta(numero, otro_numero):
+    contador =0
+    while contador< otro_numero:
+        contador = contador +1
+        suma = numero + contador
+        print( suma)
+    
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()
