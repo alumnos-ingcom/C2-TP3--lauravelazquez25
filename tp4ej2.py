6dc9d220b94afabaae55677afe526c7ac2313473
equipo <laurapamelavelazquez@gmail.com>
2021-06-02 21:03:38 -0300

    el ejercicio de la suma lenta mejorado

--- tp4ej2.py
+++ tp4ej2.py
@@ -5,15 +5,49 @@
 
 # Suma lenta
 def suma_lenta(numero, otro_numero):
-    contador =0
-    while contador< otro_numero:
-        contador = contador +1
-        suma = numero + contador
-        print( suma)
-    
+    contador = 0
+    if numero >= 0 and otro_numero >= 0:
+        while contador< otro_numero:
+            contador = contador +1
+            suma = numero + contador
+            print(suma)
+            
+    elif numero >0 and otro_numero < 0:
+        while abs(contador)< abs(otro_numero):
+            contador = contador -1
+            suma = numero + contador
+            print(suma)
+            
+    elif numero < 0 and otro_numero > 0 :
+        while contador < otro_numero:
+            contador = contador + 1
+            suma = numero + contador
+            print (suma)
+            
+    elif numero < 0 and otro_numero == 0:
+        while abs(contador)< abs(numero):
+            contador = contador +1
+            suma = otro_numero - contador
+            print(suma)        
+            
+    else:
+        while abs(contador)< abs(otro_numero):
+            contador = contador -1
+            suma = numero + contador
+            print(suma)
+        
+        
+            
 
 def prueba():
-    pass
+    """Este programa realiza la suma lenta entre dos numeros enteros,
+        mostrando las sumas parciales"""
+    
+    num1 = int(input('Ingrese un numero : '))
+    num2 = int(input('Ingrese otro numero: '))
+    print('La suma lenta es :')
+    print (suma_lenta(num1,num2))
+       
 
 if __name__ == "__main__":
     prueba()
