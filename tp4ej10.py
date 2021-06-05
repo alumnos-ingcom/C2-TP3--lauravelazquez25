9910e259ab4b51503b37398c9b7d74ed3c1908f7
equipo <laurapamelavelazquez@gmail.com>
2021-06-04 21:57:45 -0300

    ejercicio 10

---
+++ tp4ej10.py
@@ -0,0 +1,28 @@
+################
+# Laura Velazquez   @lauravelazquez25
+# Ejercicio 10
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Factores Primos
+
+def factores_primos(numero):
+    factores = []
+    
+    for i in range (2, numero):
+        while numero % i ==0 :
+            factores.append(i)
+            numero = numero / i
+    return factores
+
+
+def prueba():
+    print ('Este programa realiza la descomposicion de un numero en sus factores primos')
+    ingreso = int(input('Ingrese el numero a descomponer : '))
+     
+    print ('Los factores primos son: ', factores_primos(ingreso))
+
+if __name__ == "__main__":
+    prueba()    
+                
